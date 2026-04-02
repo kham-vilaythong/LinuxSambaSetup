66a65c6252bd375e413ea0e4670d958462982ca4
kv <39713213+kham-vilaythong@users.noreply.github.com>
2026-04-02 22:11:37 +1100

    added setup nfs setup

---
+++ nfs_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Enable exit on error and command output:
+set -eux
+echo "this sh file download nfs-utils and rpcbind and nfs-kernel-server"
+echo "download and install nfs-utils\n"
+sudo pacman -Sy --noconfirm --disable-sandbox nfs-utils rpcbind
+echo "make /nfsshare\n"
+mkdir /nfsshare
+chown nobody:nogroup /nfsshare
+echo "chmod -R 777 /nfsshare\n"
+chmod -R 777 /nfsshare
+sudo tee /etc/exports <<'EOF'
+/nfsshare *(rw,sync,no_subtree_check,insecure)
+EOF
+sudo exportfs -a
+sudo systemctl enable --now rpcbind
+echo "start nfs-server"
+sudo systemctl enable --now nfs-server.service
+echo " ubuntu sudo service nfs-kernel-server start"
+
+
+
+echo "now on the client 'mount -t nfs <serverid>:/foldershare clientfolder"
---
+++ nfs_setup_no_sudo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Enable exit on error and command output:
+set -eux
+echo "this sh file download nfs-utils and rpcbind and nfs-kernel-server"
+echo "download and install nfs-utils\n"
+pacman -Sy --noconfirm --disable-sandbox nfs-utils rpcbind
+echo "make /nfsshare\n"
+mkdir /nfsshare
+chown nobody:nogroup /nfsshare
+echo "chmod -R 777 /nfsshare\n"
+chmod -R 777 /nfsshare
+tee /etc/exports <<'EOF'
+/nfsshare *(rw,sync,no_subtree_check,insecure)
+EOF
+
+exportfs -a
+
+systemctl enable --now rpcbind
+echo "start nfs-server"
+systemctl enable --now nfs-server.service
+echo " ubuntu sudo service nfs-kernel-server start"
+
+echo "now on the client 'mount -t nfs <serverid>:/foldershare clientfolder"
